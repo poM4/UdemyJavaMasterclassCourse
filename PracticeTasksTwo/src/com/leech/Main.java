29d0b8460a2e8dd054f4b922b9ae0db67ff2474b
Igor Romanov <romaigoroleg@gmail.com>
2019-07-25 00:39:14 +0300

    adding Number of day in the month task

--- PracticeTasksTwo/src/com/leech/Main.java
+++ PracticeTasksTwo/src/com/leech/Main.java
@@ -8,7 +8,9 @@ public class Main {
 //        DayOfTheWeekChallenge.printDayOfTheWeek(6);
 //        DayOfTheWeekChallengeWithIfElse.printDayOfTheWeek(-1);
 
-        NumberInWord.printNumberInWord(10);
+//        NumberInWord.printNumberInWord(10);
+
+        System.out.println(NumberOfDayInTheMonth.getDaysInMonth(13, 2017));
 
 
 
--- PracticeTasksTwo/src/com/leech/NumberOfDayInTheMonth.java
+++ PracticeTasksTwo/src/com/leech/NumberOfDayInTheMonth.java
@@ -37,7 +37,7 @@ NOTE: Do not add a main method to solution code.
 
 public class NumberOfDayInTheMonth {
 
-    public static boolean isLeapYear (int year) {
+    public static boolean isLeapYear(int year) {
 
         if (year <= 1 || year >= 9999) {
             return false;
@@ -61,13 +61,62 @@ public class NumberOfDayInTheMonth {
 
     }
 
-    public static int getDaysInMonth (int month, int year) {
+    public static int getDaysInMonth(int month, int year) {
+
+        if ((month < 1 || month > 12) || (year < 1 || year > 9999)) {
+            return -1;
+        } else {
+            if (isLeapYear(year)) {
+                switch (month) {
+
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        return 31;
+
+                    case 2:
+                        return 29;
+
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        return 30;
+                    default:
+                        return -1;
+
+                }
+            } else {
+
+                switch (month) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        return 31;
+
+                    case 2:
+                        return 28;
+
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        return 30;
+
+                    default:
+                        return -1;
 
 
-
-        return -1;
-
-
-
+                }
+            }
+        }
     }
 }
