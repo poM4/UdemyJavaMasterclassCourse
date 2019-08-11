429f1ce0a6eb117b2b31f684063d0f9ea9b5ca3d
Igor Romanov <romaigoroleg@gmail.com>
2019-08-11 22:21:51 +0300

    adding for loop divided by 3 and 5 challenge

--- PracticeTasksTwo/src/com/leech/Main.java
+++ PracticeTasksTwo/src/com/leech/Main.java
@@ -29,6 +29,8 @@ public class Main {
 
         }
 */
+/*
+
         // Create a for statement using any range of numbers
         // Determine if the number is a prime number using the isPrime method
         // if it is a prime number, print it out AND increment a count of the
@@ -39,34 +41,61 @@ public class Main {
         int count = 0;
         for (int i = 10; i < 50; i++) {
             if (isPrime(i)) {
-                count++;
                 System.out.println("Number " + i + " is a prime number");
-                if (count == 10) {
+                count++;
+                if (count == 3) {
                     System.out.println("Exiting loop");
                     break;
                 }
             }
-
         }
-
     }
+
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        } else {
+            for (int i = 2; i <= (n / 2); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
 
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
     }
 
-    public static boolean isPrime(int n) {
-        if (n == 1) {
-            return false;
-        }
-        for (int i = 2; i <= n / 2; i++) {
-            System.out.println("Looping " + i);
-            if (n % 1 == 0) {
-                return false;
-            }
 
+*/
+
+/*
+Create a for statement using a range of numbers from 1 to 1000 inclusive.
+Sum all the numbers that can be divided with both 3 and also with 5.
+For those numbers that met the above conditions, print out the number.
+break out of the loop once you find 5 numbers that met the above conditions.
+After breaking out of the loop print the sum of the numbers that met the above conditions.
+Note: Type all code in main method
+*/
+        int count=0;
+        int sum=0;
+        for(int i=1; i<1001; i++) {
+            if((i%3==0)&&(i%5==0)) {
+                System.out.println("The number is: " + i);
+                count++;
+                sum+=i;
+                if (count==5) {
+                    System.out.println("Exiting the loop");
+                    break;
+                }
+            }
         }
-        return true;
+        System.out.println("The sum of numbers is " + sum);
+
     }
 }
-
