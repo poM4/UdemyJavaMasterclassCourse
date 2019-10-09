bc9f31e10e8116060956f84b95502b73df2e6013
Igor Romanov <igor_romanov@epam.com>
2019-10-09 19:17:30 +0300

    adding Simple Calculator task

--- PracticeTasksThree/src/com/pom4/Main.java
+++ PracticeTasksThree/src/com/pom4/Main.java
@@ -3,10 +3,24 @@ package com.pom4;
 public class Main {
 
     public static void main(String[] args) {
+/*
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         simpleCalculator.setFirstNumber(10);
         simpleCalculator.setSecondNumber(5);
         System.out.println(simpleCalculator.getAdditionResult());
+*/
+
+
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
 
     }
 }
