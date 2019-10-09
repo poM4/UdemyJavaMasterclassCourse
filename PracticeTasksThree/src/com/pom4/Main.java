2a50e7024c2e49907530a04bad5a3b87e52db574
Igor Romanov <igor_romanov@epam.com>
2019-10-09 19:36:20 +0300

    adding Person task

--- PracticeTasksThree/src/com/pom4/Main.java
+++ PracticeTasksThree/src/com/pom4/Main.java
@@ -4,14 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 /*
-        SimpleCalculator simpleCalculator = new SimpleCalculator();
-        simpleCalculator.setFirstNumber(10);
-        simpleCalculator.setSecondNumber(5);
-        System.out.println(simpleCalculator.getAdditionResult());
-*/
-
-
-
         SimpleCalculator calculator = new SimpleCalculator();
         calculator.setFirstNumber(5.0);
         calculator.setSecondNumber(4);
@@ -21,6 +13,19 @@ public class Main {
         calculator.setSecondNumber(0);
         System.out.println("multiply= " + calculator.getMultiplicationResult());
         System.out.println("divide= " + calculator.getDivisionResult());
+*/
 
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
     }
 }
