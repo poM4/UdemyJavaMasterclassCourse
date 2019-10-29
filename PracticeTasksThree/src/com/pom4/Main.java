a96ad9750819b7c625d7d1062e27c718463354fb
Igor Romanov <igor_romanov@epam.com>
2019-10-29 18:50:30 +0200

    Complex number task is done

--- PracticeTasksThree/src/com/pom4/Main.java
+++ PracticeTasksThree/src/com/pom4/Main.java
@@ -71,6 +71,7 @@ public class Main {
         System.out.println("area= " + wall.getArea());
         */
 
+/*
         Point first = new Point(6, 5);
         Point second = new Point(3, 1);
 
@@ -78,7 +79,20 @@ public class Main {
         System.out.println("distance(second)= " + first.distance(second));
         System.out.println("distance(2,2)= " + first.distance(2, 2));
         Point point = new Point();
-        System.out.println("distance()= " + point.distance());
+        System.out.println("distance()= " + point.distance());*/
+
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+
 
     }
 }
