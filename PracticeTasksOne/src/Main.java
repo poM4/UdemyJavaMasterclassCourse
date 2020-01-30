d4334c16a4bbfcb5b9f9895264b73fc6fcab2ae5
poM4 <romaigoroleg@gmail.com>
2020-01-30 08:27:53 +0200

    [Inheritance] Cylinder task

--- PracticeTasksOne/src/Main.java
+++ PracticeTasksOne/src/Main.java
@@ -1,3 +1,6 @@
+import Cylinder.Cylinder;
+import Cylinder.Circle;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -34,10 +37,19 @@ public class Main {
         MinutesToYearsDaysCalculator.printYearsAndDays(1500);
 
         EqualityPrinter.printEqual(1,1,2);
-  */
+
 
         System.out.println(PlayingCat.isCatPlaying(false, 35));
+  */
 
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
 
 
 
